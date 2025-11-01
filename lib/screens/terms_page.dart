8bb39ba0f5aa81563f451638b225e1f193ae2b7f
Zaghasen <124230097@student.upnyk.ac.id>
2025-11-01 15:45:30 +0700

    Aturan Pendakian

--- lib/screens/terms_page.dart
+++ lib/screens/terms_page.dart
@@ -50,64 +50,56 @@ class _TermsScreenState extends State<TermsScreen> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const Text(
-                        'ATURAN PENDAKIAN',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFF2A4D3A),
-                        ),
-                      ),
                       const SizedBox(height: 16),
-                      _buildRule('1.', 'pendaki wajib mengisi form registrasi'),
+                      _buildRule('1.', 'Pendaki wajib mengisi form registrasi'),
                       _buildRule(
                         '2.',
-                        'wajib meninggalkan identitas di ambil waktu turun',
+                        'Wajib meninggalkan identitas di ambil waktu turun',
                       ),
                       _buildRule(
                         '3.',
-                        'pendaki wajib mengikuti briefing atau arahan petugas',
+                        'Pendaki wajib mengikuti briefing atau arahan petugas',
                       ),
                       _buildRule('4.', 'di larang membuat jalur sendiri'),
                       _buildRule(
                         '5.',
-                        'di larang mendirikan tenda di SUN RISE VIEW',
+                        'Dilarang mendirikan tenda di SUN RISE VIEW',
                       ),
                       _buildRule(
                         '6.',
-                        'di larang merusak atau mengambil apapun milik petani di sepanjang jalur pendakian',
+                        'Dilarang merusak atau mengambil apapun milik petani di sepanjang jalur pendakian',
                       ),
                       _buildRule(
                         '7.',
-                        'sampah wajib di bawa turun sesuai yang di bawa naik',
+                        'Sampah wajib di bawa turun sesuai yang di bawa naik',
                       ),
                       _buildRule(
                         '8.',
-                        'di larang membuat api unggun atau yg lainya yg bisa mengakibatkan kebakaran',
+                        'Dilarang membuat api unggun atau yg lainya yg bisa mengakibatkan kebakaran',
                       ),
                       _buildRule(
                         '9.',
-                        'di larang membunuh hewan apapun di sepenjang jalur',
+                        'Dilarang membunuh hewan apapun di sepenjang jalur',
                       ),
                       _buildRule(
                         '10.',
-                        'di larang membuat keributan yg bisa mengganggu ketertiban umum',
+                        'Dilarang membuat keributan yg bisa mengganggu ketertiban umum',
                       ),
                       _buildRule(
                         '11.',
-                        'di larang merusak atau melubangi pipa yg ada di jalur pendakian',
+                        'Dilarang merusak atau melubangi pipa yg ada di jalur pendakian',
                       ),
                       _buildRule(
                         '12.',
-                        'di larang foto di tempat yang berbahaya',
+                        'Dilarang foto di tempat yang berbahaya',
                       ),
                       _buildRule(
                         '13.',
-                        'di larang membuang sisa makanan sembarangan (sisa makanan wajib untuk di kubur)',
+                        'Dilarang membuang sisa makanan sembarangan (sisa makanan wajib untuk di kubur)',
                       ),
                       _buildRule(
                         '14.',
-                        'pendaki wajib mengikuti peraturan yg berlaku di base camp atau masyarakat sekitar',
+                        'Pendaki wajib mengikuti peraturan yg berlaku di base camp atau masyarakat sekitar',
                       ),
                       const SizedBox(height: 16),
                       const Text(
